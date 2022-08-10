dd3005bceade3c6997eda11d189c6ce9169c182b
zayle_nothing <57306207+zayle-nothing@users.noreply.github.com>
2022-08-10 21:18:47 +0900

    Add files via upload

--- docs/java/src/BankApplication.java
+++ docs/java/src/BankApplication.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class BankApplication {
       private static Account20[] accountArray = new Account20[100];
       private static Scanner sc1 = new Scanner(System.in);
-
     public static void main(String[] args){
 
         boolean run =true;
@@ -15,7 +14,6 @@ public class BankApplication {
             System.out.print("select> ");            
             
             int SelectNo = sc1.nextInt();
-
             if(SelectNo==1){
                 createAccount();
 
@@ -37,23 +35,96 @@ public class BankApplication {
 
         sc1.close();
     }
+
     private static void createAccount(){
         String ano, owner;
         int balance;
-        ano = sc1.nextLine();
-        owner = sc1.nextLine();
+        System.out.println("------------------------------");
+        System.out.println("create account");
+        System.out.println("------------------------------");
+        System.out.printf("account number: ");
+
+        ano = sc1.next();
+        System.out.printf("owner: ");
+        owner = sc1.next();
+
+        System.out.print("Initial deposit amount: ");
         balance = sc1.nextInt();
 
-        Account20(ano,owner,balance);
-        
+        Account20 ac1 =new Account20(ano,owner,balance);
+        for(int i = 0; i<accountArray.length; i++){
+            if(accountArray[i]==null){
+            accountArray[i]=ac1;
+            System.out.println("result: account is successfully created");
+            break;
+            }
+        }
     }
+
     private static void accountList(){
-
+        System.out.println("------------------------------");
+        System.out.println("account list");
+        System.out.println("------------------------------");
+        
+        for(int i=0; i<accountArray.length;i++){
+            if(accountArray[i]==null){
+                break;
+            }else{
+                System.out.println(accountArray[i].getAno() + "\t" + accountArray[i].getOwner() + "\t" + accountArray[i].getBalance());
+            }
+        }
     }
+
     private static void deposit(){
+        System.out.println("------------------------------");
+        System.out.println("deposit");
+        System.out.println("------------------------------");
+        System.out.println("account number: ");
+        String ano = sc1.next();
+        if(findAccount(ano)==null){
+            System.out.println("amount number is not in the list");
+            return;
+        }
 
-    }
+        System.out.print("deposit amount: ");
+        int value = sc1.nextInt();
+        if (value>0){
+            findAccount(ano).setBalance(findAccount(ano).getBalance()+value);
+            System.out.println("result: deposit is successfully done");
+        }else{
+            System.out.println("deposit can't be -value");
+        }
+     }
+
     private static void withdraw(){
+        System.out.println("------------------------------");
+        System.out.println("withdraw");
+        System.out.println("------------------------------");
+        System.out.println("account number: ");
+        String ano = sc1.next();
+        if(findAccount(ano)==null){
+            System.out.println("amount number is not in the list");
+            return;
+        }
+        
+        System.out.print("withdraw amount: ");
+        int value = sc1.nextInt();
+        if ((findAccount(ano).getBalance()-value)>0){
+            findAccount(ano).setBalance(findAccount(ano).getBalance()-value);
+            System.out.println("result: withdraw is successfully done");
+        }else{
+            System.out.println("withdraw can't over the deposit amount");
+        }
+    }
 
+    private static Account20 findAccount(String ano){
+        Account20 accountTemp = null;
+        for(int i=0;i<accountArray.length;i++){
+            if((accountArray[i].getAno()).equals(ano)){
+                accountTemp=accountArray[i];
+                return accountTemp;
+            }
+        }
+        return accountTemp;
     }
 }
