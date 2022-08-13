32d402ecb1675e5c20b69e64c5be835b3375c9b5
zayle_nothing <57306207+zayle-nothing@users.noreply.github.com>
2022-08-13 23:58:47 +0900

    Add files via upload

--- docs/java/src/Member.java
+++ docs/java/src/Member.java
@@ -9,4 +9,9 @@ public class Member{
         this.name = name;
         this.id = id;
     }
+    Member(String name, String id, String password){
+        this.name = name;   
+        this.id = id;
+        this.password = password;
+    }
 }
